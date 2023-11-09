a873f26f25280d6af24f5ddb403efc4239092a95
poohcom1 <pooh677@gmail.com>
2023-11-09 10:15:05 +0700

    fix: dynamic run bar search

--- addons/run-configs/editor/lib/ui_extension.gd
+++ addons/run-configs/editor/lib/ui_extension.gd
@@ -1,17 +1,42 @@
 extends Object
 
-static func _get_editor_run_bar_container() -> Node:
-	return EditorInterface.get_base_control() \
-			.get_child(0) \
-			.get_child(0) \
-			.get_child(4) \
-			.get_child(0) \
-			.get_child(0) 
+static var _cached_editor_run_bar: Control = null
+
+## Search for the EditorRunBar by finding the parent of EditorRunNative
+## We could just search for EditorRunBar, but finding the button's parent is less likely to break in the future
+static func _get_editor_run_bar_container(root: Node = EditorInterface.get_base_control()) -> Node:
+	if _cached_editor_run_bar:
+		return _cached_editor_run_bar
+
+	for child in root.get_children():
+		if child.get_class() == &"EditorRunNative":
+			var container = child.get_parent()
+			_cached_editor_run_bar = container
+			return container
+
+		var res = _get_editor_run_bar_container(child)
+
+		if res:
+			return res
+
+	return null
 
 
 static func add_control_to_editor_run_bar(control: Control) -> void:
-	_get_editor_run_bar_container().add_child(control)
+	var run_bar := _get_editor_run_bar_container()
+	
+	if not run_bar:
+		printerr("[Run Configs] Could not find EditorRunBar. Please contact developer.")
+		return
+	
+	run_bar.add_child(control)
 
 
 static func remove_control_from_editor_run_bar(control: Control) -> void:
-	_get_editor_run_bar_container().remove_child(control)
+	var run_bar := _get_editor_run_bar_container()
+	
+	if not run_bar:
+		printerr("[Run Configs] Could not find EditorRunBar. Please contact developer.")
+		return
+	
+	run_bar.remove_child(control)
